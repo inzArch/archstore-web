a647c55b9a6062bfebb4cea9cfeb892822aba5bf
Atharva Aroskar <atharvaroskar@gmail.com>
2022-11-18 15:24:56 +0530

    merged atharva-branch into development

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,7 +3,7 @@ import CustomersFav from '../../components/Home/CustomersFav';
 import Header from '../../components/Header';
 import HomeCarousel from '../../components/Home/HomeCarousel';
 import HowItWorks from '../../components/Home/HowItWorks';
-import Brands from '../../components/Home/Brands/index';
+import Brands from '../../components/Home/Brands';
 import './index.css';
 
 const Home = () => {
