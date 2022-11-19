d79bc82ed4448c26fd064b489babd11d96a301cb
Atharva Aroskar <atharva_aroskar@archstore.in>
2022-11-19 15:21:02 +0530

    Built MegaMenu Dropdown Component

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,20 +1,30 @@
-import React from 'react';
+import React, { useState, createContext } from 'react';
 import CustomersFav from '../../components/Home/CustomersFav';
 import Header from '../../components/Header';
 import HomeCarousel from '../../components/Home/HomeCarousel';
 import HowItWorks from '../../components/Home/HowItWorks';
 import Brands from '../../components/Home/Brands/index';
 import './index.css';
+import MegaMenu from '../../components/Header/MegaMenu';
+
+export const MenuItemActiveStatusContext = createContext(null);
 
 const Home = () => {
+	const [activeMenuItem, setActiveMenuItem] = useState(null);
+
 	return (
-		<div className='Home-container'>
-			<Header />
-			<HomeCarousel />
-			<HowItWorks />
-			<CustomersFav />
-			<Brands />
-		</div>
+		<MenuItemActiveStatusContext.Provider
+			value={{ activeMenuItem, setActiveMenuItem }}
+		>
+			<div className='Home-container'>
+				<Header />
+				<MegaMenu />
+				<HomeCarousel />
+				<HowItWorks />
+				<CustomersFav />
+				<Brands />
+			</div>
+		</MenuItemActiveStatusContext.Provider>
 	);
 };
 
