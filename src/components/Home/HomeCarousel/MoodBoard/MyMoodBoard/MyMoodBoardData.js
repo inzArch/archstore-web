cee01834a066b5e245684f2d5f4607a5ae4f9193
inzamam-credencesoft <inzamam_pawaskar@archstore.in>
2022-11-21 13:10:38 +0530

    Built MoodBoard for Signed In User of Home Page

---
+++ src/components/Home/HomeCarousel/MoodBoard/MyMoodBoard/MyMoodBoardData.js
@@ -0,0 +1,23 @@
+import TribhuvanImg from '../../../../../assets/images/home/moodboard/TribhuvanImg.svg';
+import RaghavHeritageImg from '../../../../../assets/images/home/moodboard/RaghavHeritageImg.svg';
+import ForestTrailsImg from '../../../../../assets/images/home/moodboard/ForestTrailsImg.svg';
+import PushpaKamalImg from '../../../../../assets/images/home/moodboard/PushpaKamalImg.svg';
+
+export const MyMoodBoardData = [
+    {
+        img: TribhuvanImg,
+        name: "Tribhuvan Residency",
+    },
+    {
+        img: RaghavHeritageImg,
+        name: "Raghav Heritage",
+    },
+    {
+        img: ForestTrailsImg,
+        name: "Forest Trails",
+    },
+    {
+        img: PushpaKamalImg,
+        name: "PushpaKamal",
+    }
+];
