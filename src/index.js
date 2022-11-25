836466d264123901aa6ea2122a148e6a7f544582
archstore <jayesh_sawant@archstore.in>
2022-11-25 10:55:56 +0530

    Design of the month changes, other components commented in index.js

--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import App from './App';
+// import App from './App';
 // import BrandsListingPage from './pages/BrandsListingPage';
-// import Carousel from './components/EmblaCarousel';
+ import Carousel from './components/EmblaCarousel';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
-		<App />
+		{/* <App /> */}
 		{/* <CoverPage /> */}
 		{/* <BrandsListingPage /> */}
-		{/* <Carousel /> */}
+		<Carousel />
 	</React.StrictMode>
 );
